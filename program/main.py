22cdc17e89e47279b7710b307691e44bcc32506f
MiaoWenKai <49262721+MiaoWenKai@users.noreply.github.com>
2021-02-06 23:56:29 +0800

    人脸识别由dlib改为ultra_light_fast_generic_face_detector_1mb_640；修复了画面和检测结果不同步的问题，图像显示之后，再分配下一帧图像

--- program/main.py
+++ program/main.py
@@ -7,8 +7,8 @@ import time
 from multiprocessing import Process, Queue
 import uav
 import keyboard
-import dlib
 import numpy as np
+import pid
 
 
 def uav_control(q):
@@ -20,28 +20,52 @@ def uav_control(q):
     minifly.control_start(q)
 
 
+def cap_cache(cap_queue, flag_queue, frame_queue, progress_count):  # 捕获队列，标志队列，图像队列
+    """图像分配进程"""
+    cache = None
+    flag = 1
+    start_time = time.time()
+    while 1:
+        try:
+            cache = cap_queue.get_nowait()
+        except Exception:pass
+        try:
+            flag_queue.get_nowait()
+            flag = 1
+            start_time = time.time()
+        except Exception:pass
+        if flag == 1 and type(cache) is np.ndarray:
+            print(f"2图像分配进程等待接收图片时间：{time.time()-start_time}s")
+            for _ in range(progress_count + 1):
+                frame_queue.put(cache)
+            cache = None
+            flag = 0
+
+
 def capture(cap_q):
     """图像捕获进程"""
     WIFI = wifi_init.wifi()  # 实例化wifi类
     wifi_init.camera_connect(WIFI)  # 连接飞行器摄像头
     while True:
+        start_time = time.time()
         wifi_init.check_connection(WIFI)  # 检查wifi连接状态
         cap = cv2.VideoCapture('http://192.168.1.1:80/snapshot.cgi?user=admin&pwd=')
         if cap.isOpened():
             ret, frame = cap.read()
             frame = imutils.resize(frame, width=640)  # 缩小图片尺寸
             cap_q.put(frame)
-            print(f"1捕获进程接收图片时间{time.time()}")
+            print(f"1捕获进程接收图片时间:{time.time() - start_time}s")
 
 
-def object_detector(frame_queue, obj_queue, flag_queue):
+def object_detector(frame_queue, obj_queue):
     """物体检测进程"""
     # 加载模型，可选模型 ssd_mobilenet_v1_pascal 和 yolov3_mobilenet_v1_coco2017
     object_detector = hub.Module(name="yolov3_mobilenet_v1_coco2017")
-    flag_queue.put(1)
+    start_time = time.time()
     while True:
         frame = frame_queue.get()
-        print(f"3物体检测进程接收时间：{time.time()}")
+        print(f"3物体检测进程接收等待时间：{time.time()-start_time}s")
+        start_time = time.time()
         outputs = object_detector.object_detection(
             images=[frame],
             batch_size=1,
@@ -50,81 +74,89 @@ def object_detector(frame_queue, obj_queue, flag_queue):
             visualization=False
         )
         obj_queue.put(outputs)
-        flag_queue.put(1)
+        print(f"3物体检测完成用时：{time.time() - start_time}s")
+        start_time = time.time()
 
 
-def face_detector(frame_queue, face_queue, flag_queue):
+def face_detector(frame_queue, face_queue):
     """人脸检测进程"""
-    detector = dlib.get_frontal_face_detector()  # 获取人脸分类器
-    flag_queue.put(1)
+    face_detector = hub.Module(name="ultra_light_fast_generic_face_detector_1mb_640")
+    start_time = time.time()
     while True:
         frame = frame_queue.get()
-        print(f"4人脸检测进程接收时间：{time.time()}")
-        dets = detector(frame, 1)  # 使用detector进行人脸检测 dets为返回的结果
-        face_queue.put(dets)
-        flag_queue.put(1)
+        print(f"4人脸检测进程接收等待时间：{time.time()-start_time}")
+        start_time = time.time()
+        result = face_detector.face_detection(
+            images=[frame],
+            use_gpu=False,
+            visualization=False,
+            confs_threshold=0.5)
+        face_queue.put(result)
+        print(f"4人脸检测完成用时：{time.time() - start_time}")
+        start_time = time.time()
 
 
-def show_img(frame_queue, obj_queue, face_queue):
+def show_img(frame_queue, flag_queue, ctrl_msg_queue, obj_queue, face_queue):
     """图像显示进程"""
-    time_record = time.time()
+    Roll_pid = pid.PID(0.01, 0, 0, 2)
+    Pitch_pid = pid.PID(0.1, 0, 0, 2)
+    Height_pid = pid.PID(0.2, 0, 0, 0)
+    Yaw_pid = pid.PID(0, 0, 0, 0)
+    face_size = 0
+    start_time = time_record = time.time()
     while 1:
         frame = frame_queue.get()
-        print(f"8图像显示进程接收{time.time()}")
+        print(f"8图像显示进程等待{time.time()-start_time}s")
+        start_time = time.time()
         """物体检测数据处理"""
         obj_res = obj_queue.get()
-        print(obj_res)
         for data in obj_res[0]["data"]:
             left = int(data["left"])
             top = int(data["top"])
             right = int(data["right"])
             bottom = int(data["bottom"])
             cv2.rectangle(frame, (left, top), (right, bottom), (255, 100, 100), 2)
-            cv2.putText(frame, f"{data['label']}", (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        (43, 43, 43), 1)
+            cv2.putText(frame, f"{data['label']}", (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (43, 43, 43), 1)
         """人脸识别数据处理"""
-        dets = face_queue.get()
-        print("Number of faces detected: {}".format(len(dets)))  # 打印识别到的人脸个数
-        for index, face in enumerate(dets):
-            print('face {}; left {}; top {}; right {}; bottom {}'.format(index, face.left(), face.top(), face.right(),
-                                                                         face.bottom()))
-            # 在图片中标注人脸，并显示
-            left = face.left()
-            top = face.top()
-            right = face.right()
-            bottom = face.bottom()
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        face_ret = face_queue.get()
+        for face in face_ret[0]["data"]:
+            left = int(face["left"])
+            top = int(face["top"])
+            right = int(face["right"])
+            bottom = int(face["bottom"])
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)   # 在图片中标注人脸，并显示
+        """识别结果接收完成，标志位置1"""
+        # flag_queue.put(1)
+        """自动飞行控制"""
+        # if len(face_ret[0]["data"]) > 0:
+        #     center = frame.shape[1]/2
+        #     x_err = (right + left)/2 - center
+        #     roll_ctrl = Roll_pid.update(x_err)
+        #
+        #     center = frame.shape[0] / 2
+        #     z_err = (bottom + top)/2 - center
+        #     height_ctrl = Height_pid.update(-z_err)
+        #
+        #     if face_size == 0:
+        #         face_size = (right+bottom) - (left+top)
+        #         pitch_err = 0
+        #     else:
+        #         pitch_err = face_size/((right+bottom) - (left+top)) - 1
+        #     pitch_ctrl = Pitch_pid.update(pitch_err)
+        #
+        #     ctrl_msg_queue.get([roll_ctrl, pitch_ctrl, 0, height_ctrl])
+
         """图像显示"""
         fps = 1 / (time.time() - time_record)
         time_record = time.time()
-        cv2.putText(frame, f"FPS:{int(fps)} ", (550, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    (0, 0, 0), 1)
+        cv2.putText(frame, f"FPS:{int(fps)} ", (550, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
         # frame = imutils.resize(frame, width=1536)  # 放大图片尺寸
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == 27:  # 按ESC退出
             break
-
-
-def cap_cache(cap_queue, flag_queue, frame_queue, progress_count):  # 捕获队列，标志队列，图像队列，进程数量
-    """图像分配进程"""
-    cache = None
-    flag = 0
-    while 1:
-        try:
-            cache = cap_queue.get_nowait()
-        except Exception:
-            pass
-        try:
-            flag_queue.get_nowait()
-            flag += 1
-        except Exception:
-            pass
-        if type(cache) is np.ndarray and flag == progress_count:
-            for _ in range(progress_count + 1):
-                frame_queue.put(cache)
-            cache = None
-            flag = 0
-            print(f"2图像分配进程发送图片时间：{time.time()}")
+        print(f"8图像显示用时：{time.time() - start_time}s")
+        start_time = time.time()
+        flag_queue.put(1)
 
 
 if __name__ == '__main__':
@@ -134,18 +166,19 @@ if __name__ == '__main__':
     cap_queue = Queue(True)  # 捕获图像队列
     ctrl_msg_queue = Queue(True)  # 控制信号队列
     frame_queue = Queue(True)  # 图像队列
+    # show_queue = Queue(True)  # 显示图像队列
     obj_queue = Queue(True)  # 物体检测结果队列
     face_queue = Queue(True)  # 人脸识别结果队列
     flag_queue = Queue(True)  # 标志队列
     """进程创建"""
     Uav_control = Process(target=uav_control, args=(ctrl_msg_queue,))  # 无人机控制进程
     Capture = Process(target=capture, args=(cap_queue,))  # 捕获图像进程
-    Obj_detector = Process(target=object_detector, args=(frame_queue, obj_queue, flag_queue))  # 物体检测进程
-    Face_detector = Process(target=face_detector, args=(frame_queue, face_queue, flag_queue))  # 人脸检测进程
-    Show_img = Process(target=show_img, args=(frame_queue, obj_queue, face_queue))  # 图像显示进程
     Cap_cache = Process(target=cap_cache, args=(cap_queue, flag_queue, frame_queue, img_process_num))  # 图像分配进程
-    # 启动子进程
-    Uav_control.start()  # 无人机控制进程
+    Obj_detector = Process(target=object_detector, args=(frame_queue, obj_queue))  # 物体检测进程
+    Face_detector = Process(target=face_detector, args=(frame_queue, face_queue))  # 人脸检测进程
+    Show_img = Process(target=show_img, args=(frame_queue, flag_queue, ctrl_msg_queue, obj_queue, face_queue))  # 图像显示进程
+    """启动子进程"""
+    # Uav_control.start()  # 无人机控制进程
     Capture.start()  # 捕获图像进程
     Show_img.start()  # 图像分配进程
     Obj_detector.start()  # 物体检测进程
